40bb651f4c4abb7a36cabc16d97658759d736d6c
Michał Wąsowicz <mwasowicz7@gmail.com>
2019-05-28 22:04:49 +0200

    board endpoint

--- backend/src/main/java/io/bearcave/yakba/rest/BoardsController.java
+++ backend/src/main/java/io/bearcave/yakba/rest/BoardsController.java
@@ -1,12 +1,17 @@
 package io.bearcave.yakba.rest;
 
+import io.bearcave.yakba.exceptions.BadRequest;
 import io.bearcave.yakba.models.Board;
 import io.bearcave.yakba.services.BoardService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/board")
@@ -25,7 +30,20 @@ public class BoardsController {
     }
 
     @PostMapping
-    public void createNewBoard(@RequestBody Board board) {
+    public Mono<Board> createNewBoard(
+            @RequestBody Map<String, String> body,
+            Principal user) {
+        var boardName = body.get("name");
+        if (StringUtils.isEmpty(boardName)) {
+            throw new BadRequest();
+        }
 
+        return boardService.createNewBoard(body.get("name"), user.getName());
+    }
+
+    @DeleteMapping(value = "/{boardId}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteBoardByOwner(@PathVariable("boardId") String boardId, Principal user) {
+        return boardService.deleteBoard(boardId, user.getName());
     }
 }
--- backend/src/main/java/io/bearcave/yakba/services/BoardService.java
+++ backend/src/main/java/io/bearcave/yakba/services/BoardService.java
@@ -1,36 +1,55 @@
 package io.bearcave.yakba.services;
 
-import io.bearcave.yakba.dao.BoardAccessRepository;
 import io.bearcave.yakba.dao.BoardRepository;
-import io.bearcave.yakba.dto.UserBoardAccessDTO;
+import io.bearcave.yakba.exceptions.Forbidden;
 import io.bearcave.yakba.models.Board;
+import io.bearcave.yakba.models.BoardAccessLevel;
 import io.bearcave.yakba.models.UserBoardAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
 
-import java.util.function.Function;
+import java.util.Objects;
 
 @Service
 public class BoardService {
 
     private final BoardRepository boardRepository;
-    private final BoardAccessRepository boardAccessRepository;
 
     @Autowired
-    public BoardService(BoardRepository boardRepository, BoardAccessRepository boardAccessRepository) {
+    public BoardService(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
-        this.boardAccessRepository = boardAccessRepository;
     }
 
     public Flux<Board> getBoardsForUser(String userId) {
-        return Flux.mergeSequential(boardAccessRepository.findAllByUserId(userId)
-                .map(userBoardAccess -> boardRepository.findById(userBoardAccess.getBoardId())));
+        return boardRepository.findAll()
+                .filter(board -> board.getAccesses()
+                        .stream()
+                        .map(UserBoardAccess::getUserId)
+                        .anyMatch(accessUserId -> Objects.equals(accessUserId, userId)));  // FIXME: optimize
     }
 
-    public Mono<Board> createNewBoard(Board board, String userId) {
-        return boardRepository.insert(board);
+    public Mono<Board> createNewBoard(String boardName, String userId) {
+        return boardRepository.insert(new Board(boardName, userId));
+    }
+
+    public Mono<Void> deleteBoard(String boardId, String ownerId) {
+        return boardRepository.findById(boardId)
+                .map(board -> {
+                    if (!hasUserAdminAccess(board, ownerId)) {
+                        throw new Forbidden();
+                    }
+
+                    return Mono.empty();
+                })
+                .then();
+    }
+
+    private boolean hasUserAdminAccess(Board board, String userId) {
+        return board.getAccesses()
+                .stream()
+                .filter(access -> Objects.equals(access.getAccessLevel(), BoardAccessLevel.ADMIN))
+                .anyMatch(access -> Objects.equals(access.getUserId(), userId));
     }
 }
