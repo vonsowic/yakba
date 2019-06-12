c6eb5994eaa95d91d64d37589b95a0dda8b8cd11
Michał Wąsowicz <mwasowicz7@gmail.com>
2019-06-12 20:34:30 +0200

    remove method

--- frontend/src/app/services/navigation.service.ts
+++ frontend/src/app/services/navigation.service.ts
@@ -1,15 +1,25 @@
 import {Injectable} from '@angular/core';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor(private router: Router) {
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
   }
 
   goToNotFound(): Promise<any> {
     return this.router.navigateByUrl('/not-found');
+  }
+
+  getCurrentBoardId(): Observable<string> {
+    return this.route.params
+      .pipe(
+        map(params => params['boardId'])
+      )
   }
 }
