045c0bbf754e3bc9952cb9049f32cada31374465
Michał Wąsowicz <mwasowicz7@gmail.com>
2019-06-22 16:16:36 +0200

    refactor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements OnInit {
 
-  private username: String;
+  private username: string;
 
   constructor(private authService: AuthService, private router: Router) {
 
